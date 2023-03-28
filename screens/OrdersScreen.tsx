561b2b105a844a14a3e4358d67e0de7a745381a4
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-03-28 04:53:31 +0200

    Order-Page

--- screens/OrdersScreen.tsx
+++ screens/OrdersScreen.tsx
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, ActivityIndicator } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react';
+import { CompositeNavigationProp, RouteProp, useNavigation } from '@react-navigation/native';
+import { RootStackParamsList } from '../navigator/RootNavigator';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'; 
+import { useTailwind } from 'tailwind-rn/dist';
+import useOrders from '../hooks/useOrders';
+import { Button, Image } from '@rneui/themed';
+
+export type OrderScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, "Orders">,
+  NativeStackNavigationProp<RootStackParamsList>
+>;
 
 const OrdersScreen = () => {
-  return (
-    <View>
-      <Text>This is the orders screen</Text>
-    </View>
-  )
-}
+  const tw = useTailwind();
+  const navigation =useNavigation<OrderScreenNavigationProp>();
+  const { loading, error, orders } = useOrders()
+  const [ascending, setAscending] = useState<boolean>(false)
 
-export default OrdersScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      tabBarLabel: ({ focused, color }) => (
+        <Text style={{ color: focused ? "red" : color, fontSize: 10}}>
+          Orders
+        </Text>
+      ),
+    });
+  }, []);
+
+  return (
+    <ScrollView style={{ background: "#EB6A7C"}}>
+      <Image source={{ uri: "https://links.papareact.com/m51"}} 
+        containerStyle={tw("w-full h-64")}
+        PlaceholderContent={<ActivityIndicator />}
+      />  
+
+      <View>
+        <Button 
+          color="pink"
+          titleStyle={{ color: "gray", fontweight: "400"}}
+          style={tw("py-2 px-5")}
+          onPress={() => setAscending(!ascending)}>
+          {ascending ? "Showing: Oldest First" : "Showing: Most Recent First"}
+        </Button>
+
+        {orders?.sort((a,b) => {
+          if (ascending) {
+            return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+          } else {
+            return new Date(a.createdAt) < new Date(b.createdAt) ? 1 : -1;
+          }
+        }).map(order => (
+          <OrderCard key={order.trackingId} item={order} />
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default OrdersScreen;
